a68bf0123f1ea849e5fd1aaf12ba93c0cd2ab8d7
sandhya-1703 <sandhyamurugan0508@gmail.com>
2026-05-02 19:36:35 +0530

    fix start script

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_URI)
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
 const auth = require("./middleware/auth"); // ✅ middleware
 
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // ✅ Middleware
 app.use(cors());
